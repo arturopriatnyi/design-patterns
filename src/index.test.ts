a4cdc18051226a3b127f072daa8f3d6686393949
artur.opriatnyi <imarrche@gmail.com>
2022-02-19 07:08:12 +0200

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { APP_NAME } from './index';
+
+describe('App', () => {
+  it('APP_NAME is "Design patterns"', () => {
+    expect(APP_NAME).toBe('Design patterns');
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = 'Design patterns';
+
+console.log(APP_NAME);
